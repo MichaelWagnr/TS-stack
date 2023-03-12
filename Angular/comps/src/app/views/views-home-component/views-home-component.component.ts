c1229402f9cf5f04281ee68b8e731483f8b88cbe
Michael Wagner <michaelwagner780@gmail.com>
2023-03-12 15:11:13 -0400

    statistics component

--- Angular/comps/src/app/views/views-home-component/views-home-component.component.ts
+++ Angular/comps/src/app/views/views-home-component/views-home-component.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-views-home-component',
   templateUrl: './views-home-component.component.html',
-  styleUrls: ['./views-home-component.component.css']
+  styleUrls: ['./views-home-component.component.css'],
 })
 export class ViewsHomeComponentComponent {
-
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' },
+  ];
 }
--- Angular/comps/src/app/views/views.module.ts
+++ Angular/comps/src/app/views/views.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponentComponent } from './views-home-component/views-home-component.component';
 import { StatisticsComponent } from './statistics/statistics.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ViewsHomeComponentComponent,
-    StatisticsComponent
-  ],
-  imports: [
-    CommonModule,
-    ViewsRoutingModule
-  ]
+  declarations: [ViewsHomeComponentComponent, StatisticsComponent],
+  imports: [CommonModule, ViewsRoutingModule, SharedModule],
 })
-export class ViewsModule { }
+export class ViewsModule {}
