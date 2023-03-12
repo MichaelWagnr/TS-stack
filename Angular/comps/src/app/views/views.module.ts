3fae9cea4f233407c7537d45e49eda45deab46b7
Michael Wagner <michaelwagner780@gmail.com>
2023-03-12 15:02:56 -0400

    views module exercise

--- Angular/comps/src/app/views/views.module.ts
+++ Angular/comps/src/app/views/views.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewsHomeComponentComponent } from './views-home-component/views-home-component.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewsHomeComponentComponent,
+    StatisticsComponent
+  ],
   imports: [
     CommonModule,
     ViewsRoutingModule
